e1b98df63a042630068e7dfc3140bcf3be46a5bf
Aritra Karmakar <aritrak02@gmail.com>
2023-05-27 10:35:29 +0530

    first commit YelpCamp

---
+++ schemas.js
@@ -0,0 +1,11 @@
+const Joi = require ('joi');
+
+module.exports.campgroundSchema = Joi.object({ //this is not a mongoose schema but a joi schema which is used for validation of data before sending it to mongoose schema 
+    campground: Joi.object({
+        title: Joi.string().required(),
+        price: Joi.number().required().min(0),
+        image: Joi.string().required(),
+        location: Joi.string().required(),
+        description: Joi.string().required()
+    }).required()
+})
